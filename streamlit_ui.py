f014635133e7205b23fd4dde8fc5e427e4fc6a12
Mohammad <mohammad70623@gmail.com>
2026-03-04 16:05:21 +0600

    add chat history using session_state

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -3,3 +3,11 @@ from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
 
 CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
