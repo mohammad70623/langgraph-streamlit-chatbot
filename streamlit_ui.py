60dcfcd12beedb8073772a9080e20bec78897afe
Mohammad <mohammad70623@gmail.com>
2026-03-04 16:09:02 +0600

    add user chat input

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -11,3 +11,11 @@ if 'message_history' not in st.session_state:
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
+    
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
